c76a09f16014c05893a76764037e4e826f821a89
alvin <alvinngow21@gmail.com>
2023-04-05 20:25:07 +0800

    test webapp

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -3,8 +3,30 @@ import './App.css';
 import { useRef } from 'react';
 
 function App() {
-	function runDemo() {
+	async function runDemo() {
 		console.log(inputRef.current.value);
+		const sentence = inputRef.current.value;
+		const response = await fetch(
+			`http://127.0.0.1:9001/lda_demo?sentence=${sentence}`,
+			{
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json;charset=UTF-8',
+					'Access-Control-Allow-Origin': '*',
+				},
+			}
+		);
+		return response;
+	}
+
+	async function handleClick() {
+		const response = await runDemo();
+		const res = await response.json();
+		console.log(
+			'🚀 ~ file: App.js:25 ~ handleClick ~ response:',
+			await response.json()
+		);
 	}
 
 	const inputRef = useRef();
@@ -25,12 +47,23 @@ function App() {
 					</div>
 					<button
 						className='bg-blue-500 mt-2 p-2 rounded-3xl'
-						onClick={runDemo}
+						onClick={handleClick}
 					>
 						Analyse!
 					</button>
 				</div>
 			</header>
+			<div className='App-header mt-12'>
+				<h2 className='text text-5xl'>LDA</h2>
+				<div class='grid grid-cols-4 mt-2'>
+					<div>
+						<h2>Untuned LDA</h2>
+					</div>
+					<div>Predicted Topic</div>
+					<div>Tuned LDA</div>
+					<div>Predicted Topic</div>
+				</div>
+			</div>
 		</div>
 	);
 }
